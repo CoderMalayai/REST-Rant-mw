9f16a741b7a0a15b384f1adb9f523c2ff9482ba1
Malayai Wilson <147570954+CoderMalayai@users.noreply.github.com>
2024-02-05 18:34:11 -0800

    Added 404 page

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello Rest-Rant World1!')
+    res.send('Hello Rest-Rant World!');
+});
+
+// wildcard/ 404 route
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404 Page</h1>');
 });
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running at http://localhost:${process.env.PORT}`);
-})
+});
