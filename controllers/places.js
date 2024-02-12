9c2b4d5de5bb7edf313745c8a39eaa5d01370099
Malayai Wilson <147570954+CoderMalayai@users.noreply.github.com>
2024-02-12 01:24:57 -0800

    Delete functionality implemented

--- controllers/places.js
+++ controllers/places.js
@@ -44,4 +44,22 @@ router.get ('/:id', (req, res) => {
 })
 // Show page //
 
+// Delete page //
+
+router.delete('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    }
+    else if (!places[id]) {
+        res.render('error404')
+    }
+    else {
+        places.splice(id, 1)
+        res.redirect('/places')
+    }
+})
+
+// Delete page //
+
 module.exports = router
