658afc886a97a5a19afb580867a02d3179563259
Malayai Wilson <147570954+CoderMalayai@users.noreply.github.com>
2024-02-12 01:08:59 -0800

    Edit and delete

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,11 +1,37 @@
 const React = require ('react')
 const Def = require('../default')
 
-function show () {
+function show (data) {
     return (
         <Def>
+            <head>
+                <link rel="stylesheet" href="/style.css"/>
+            </head>
             <main>
-                <h1>Show Page</h1>
+                <div class="container text-center">
+                    <div class="row align-items-start">
+                        <div class="col">
+                            <img src={data.place.pic} alt={data.place.name}/>
+                        </div>
+                        <div class="col">
+                            <h1>{data.place.name}</h1>
+                            <h2>Rating</h2>
+                            <p>Not Rated</p>
+                            <h2>Description</h2>
+                            <p></p>
+                        </div>
+                    </div>
+                </div>
+                <h2>Comments</h2>
+                <p>No comments yet!!!</p>
+                <a href={`/places/${data.id}/edit`} className="btn btn-warning">
+                    Edit
+                </a>
+                <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+                    <button type="Submit" className="btn btn-danger">
+                        Delete
+                    </button>
+                </form>
             </main>
         </Def>
     )
